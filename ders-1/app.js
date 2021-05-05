c6f5731a6067c3f811a50240f4386d8d1b010e8d
kamilsmr <kamils6035@gmail.com>
2021-05-05 03:44:43 +0300

    start

---
+++ ders-1/app.js
@@ -0,0 +1,9 @@
+      document.write("hello!\n"); // alt satıra indirmedi cunku html tagları ile yazmamız lazım o yuzden alt satırda br ile yazacağız
+      document.write("Clarusway");
+      document.write('hello! <br>');
+      document.write('Clarusway');
+      console.log("hello!\n"); // consolda yazdı cunku htmlde degıl
+      console.log("Clarusway");
+      alert('hello\nClarusway');
+      confirm('hello');
+      prompt("yaşınız:", 17)// eger yas girmezse 17 yazacak
