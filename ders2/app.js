13c167c9bd14ddab1bd290961ade2b9a138acf40
kamilsmr <kamils6035@gmail.com>
2021-05-05 16:29:47 +0300

    continue

--- ders2/app.js
+++ ders2/app.js
@@ -2,4 +2,36 @@
 // var count = 10;
 
 // console.log('from outside');
-var a = 3; // window.a diyerek a yı bulabiliyoruz. this.a da kullanılabilir
+// var a = 3; // window.a diyerek a yı bulabiliyoruz. this.a da kullanılabilir
+
+// var b = 6 ;
+// console.log(b);
+
+// var b;
+// console.log(b); //undefıne verdı cunku deger ataması yapmadık.
+// b = 6;
+// console.log(b);
+
+// var c = ' '; // undefine vereceğine boş değer verin.
+
+// eger hıc tanımlamadıysak is not defined diyecek
+
+// use strict eger bu yazılı olursa  hata  verecektir.
+// s6 ile artık aşağıdaki ifadeyi anlayabiliyor.
+// b=6;
+// console.log(b)
+
+// var x;
+// console.log(x)
+// //undefined hatadı verecktir.
+
+// var x=5;
+// var y=7;
+// y=x;
+// console.log(x);
+// console.log(y);
+
+// atamalar sağdan sola yapılır önemli
+
+// 4ahmet, your name, they-surname bu ifadeler yanlış değişkenler için
+// first_7, value9a, $ahmet bu ifadeler doğrudur. _ ve $ başta kullanabiliriz.
