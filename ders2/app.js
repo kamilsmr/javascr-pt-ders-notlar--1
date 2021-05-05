a55fb0260db833913148305b21c0aebc099e59fd
kamilsmr <kamils6035@gmail.com>
2021-05-05 06:22:30 +0300

    ders-2

---
+++ ders2/app.js
@@ -0,0 +1,5 @@
+// ilk değer ataması yapıp değişken atamak önemlidir.Çünkü değer atanmamış değişkenlere hafıza ayrılıyor.
+// var count = 10;
+
+// console.log('from outside');
+var a = 3; // window.a diyerek a yı bulabiliyoruz. this.a da kullanılabilir
