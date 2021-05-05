2dc9d963380b1eb8be54c73ab1d06f8109137fac
kamilsmr <kamils6035@gmail.com>
2021-05-05 05:34:08 +0300

    update

--- ders-1/app.js
+++ ders-1/app.js
@@ -24,10 +24,29 @@
 // değişken isimleri--değişken iismleri farklı olacak
 //değişken isimleri -- boşluk olmayacak, emoji kullanılamaz
 // türkçe karakter kullanılabilir, fakat ingilizce koymaya çalışalım.
+// boşluk kullandığımızda yeni bir eleman gibi algılıyor.
+// var ali; // olur
+// var 1a; //rakamla başlayamaz.
 
-var ali; // olur
-var 1a; //rakamla başlayamaz
+// console.log([1,2,3,4]);
+// console.table([1,2,3,4]);
+// console.log({a:1,b:2,c:3,d:4});
+// console.table({a:1,b:2,c:3,d:4});
 
+// console ile ilgili mdn e bakılabilir kullanım alanı çok büyük
+// console.time('event'); // time ile timeEnd kullanarak ne kadar sürdüğünü öğrenebiliriz.
+// console.log('message');
+// console.info('some info');
+// console.error('some error');
+// console.warn('some warning');
+// console.timeEnd('event')
+   
+
+// console.group(); // grup olarak gösteriyoruz
+// console.groupEnd();
+// var country = prompt('Enter your country :');
+// alert('your country ' + country);
+// console.log('your country ' + country);
 
 
 
