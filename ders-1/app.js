4aee760c3c83061bd030ac81e4ab00edd27a3288
kamilsmr <kamils6035@gmail.com>
2021-05-05 04:08:05 +0300

    uptade

--- ders-1/app.js
+++ ders-1/app.js
@@ -1,9 +1,21 @@
-      document.write("hello!\n"); // alt satıra indirmedi cunku html tagları ile yazmamız lazım o yuzden alt satırda br ile yazacağız
-      document.write("Clarusway");
-      document.write('hello! <br>');
-      document.write('Clarusway');
-      console.log("hello!\n"); // consolda yazdı cunku htmlde degıl
-      console.log("Clarusway");
-      alert('hello\nClarusway');
-      confirm('hello');
-      prompt("yaşınız:", 17)// eger yas girmezse 17 yazacak
+// document.write("hello!\n"); // alt satıra indirmedi cunku html tagları ile yazmamız lazım o yuzden alt satırda br ile yazacağız
+// document.write("Clarusway");
+// document.write("hello! <br>");
+// document.write("Clarusway");
+// console.log("hello!\n"); // consolda yazdı cunku htmlde degıl
+// console.log("Clarusway");
+// alert("hello\nClarusway");
+// confirm("hello");
+// prompt("yaşınız:", 17); // eger yas girmezse 17 yazacak
+
+// document.body.innerHTML = "<h1> Merhaba Dünya!</h1>";
+// eger script i head kısmında yazarsak olmuyor. paragraf vb oluşturulmadıysa onları değiştiremez. Bu yüzden bodynin altına yazılır genelde.
+// meta data da özellikleri şu zamnda oluşturuldu vb tarifler meta data da verilir.
+// çift ve tek tırnak sorun yapmıyor ama eslin tek tırnak öneriyor.
+
+// document.body.innerHTML = "Let's start coding!";
+document.body.innerHTML = 'Let\'s start coding!';
+
+
+
+
