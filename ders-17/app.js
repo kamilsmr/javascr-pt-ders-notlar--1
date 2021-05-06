0daff004c17d39a259d780a59558cdc553271154
kamilsmr <kamils6035@gmail.com>
2021-05-07 02:22:42 +0300

    uptade

--- ders-17/app.js
+++ ders-17/app.js
@@ -1,26 +1,26 @@
-* eslint-disable arrow-body-style */
+// * eslint-disable arrow-body-style */
 
-const renderCountry = (data, className = '') => {
-  const countryElm = document.querySelector('.countries');
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countryElm.insertAdjacentHTML('beforeend', html);
-  countryElm.style.opacity = 1;
-};
+// const renderCountry = (data, className = '') => {
+//   const countryElm = document.querySelector('.countries');
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countryElm.insertAdjacentHTML('beforeend', html);
+//   countryElm.style.opacity = 1;
+// };
 
-// // AJAX Calls
+// // AJAX Callss
 // // XMLHttpRequest - XHR
 // // old way, not common anymore
 // const getCountryData = (country) => {
@@ -36,4 +36,35 @@ const renderCountry = (data, className = '') => {
 // };
 
 // getCountryData('turkey');
-© 2021 GitHub, Inc.
+
+// const request = fetch("https://retscountries.eu/rest/v2/name/turkey");
+// console.log(request);
+
+// request
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     renderCountry(data[0]);
+//   });
+
+const getCountryData = (country) => {
+    fetch(`https://retscountries.eu/rest/v2/name/${country}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        renderCountry(data[0]);
+
+
+    });
+};
+
+getCountryData('Turkey')
+getCountryData('Germany')
+getCountryData('Canada')
+
+
+
