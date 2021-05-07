d3cd5e071b4bcc5165b02357e31bfdcfa00fe44b
kamilsmr <kamils6035@gmail.com>
2021-05-07 04:39:39 +0300

    uptade

---
+++ ders-3/app.js
@@ -0,0 +1,27 @@
+// dinamik, değişkenin tipini ve değerini değiştirebiliyoruz.
+// primitive değerlerin iç değiştirilemez. 
+// en son giren ilk çıkıyor. Tabak örneği
+// myVar1 = "hello" // hello kısmı literal kısmı
+// myVar2 = myVar1; // bu durumda myVar1 in değeri değişse de myVar2 nin değerini değiştrmez.
+// Farklı bellekte o yuzden değişmez.
+//stackoverflow yığınlar bellekte doluyor.  
+// heap valulerin depolandığı yerdir. iadesini yapmam lazım işim bitince.
+// c ve c++ da bellek yönetimini siz yapıyorsunuz. Eğer sen bellekteki veriyi iptal etmezsen bellek dolu oluyor ve yavaşlıyor.
+// bazı dillerde stringler heap de muhafaza ediliyor.
+// javascript heap belleğiyle ilgileniyor. ve biz null dediğimizde siliyor.  
+// arrayi heap te tutuyor. çünkü ne kadar yer tutacağı belli değil.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
