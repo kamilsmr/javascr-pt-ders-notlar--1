fc5e0ef6569b37f6c66359632e329924ed54892a
kamilsmr <kamils6035@gmail.com>
2021-05-11 08:44:22 +0300

    uptade

---
+++ Kosullu-ifadeler/app.js
@@ -0,0 +1,18 @@
+// let x='13' +'4'; //sonuç 134
+// console.log(x);
+
+// x = 8+'12';
+// console.log(x); // 812
+
+// x= '20'-'12';
+// console.log(x); // 8 çıktı
+
+// // ASCII tablosuna göre stringlerin bir değeri vardır.
+
+// x = "A".charCodeAt(0);
+// console.log(x);
+
+// comma operatörünü detaylı bak
+// let x = (y=5 , z=6);
+// console.log(x); // 6 döndü virgül ensondaki değeri döndürür.
+
