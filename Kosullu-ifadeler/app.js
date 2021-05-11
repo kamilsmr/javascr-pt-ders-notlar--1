a81294657157fe998eecb045c561490ac0335766
kamilsmr <kamils6035@gmail.com>
2021-05-11 11:24:25 +0300

    uptade

--- Kosullu-ifadeler/app.js
+++ Kosullu-ifadeler/app.js
@@ -94,9 +94,115 @@
 
 // sadece return dersek çıkıyor. değer verirsek değer verip çıkıyor.
    
+// İF ELSE EXAMPLES
+
+// const number1 = prompt('Enter a number: ');
+// if (number1 > 0) {
+//   console.log('The number is positive');
+// }
+// console.log('The if statement is easy');
 
 
 
+// const number2 = prompt('Enter a number: ');
+// if (number2 > 0) {
+//   console.log('The number is positive');
+// } else { 
+//   console.log('The number is either a negative number or 0');
+// }
+// console.log('The if...else statement is easy');
+
+
+
+// const number3 = prompt('Enter a number: ');
+// if (!isNaN(number3) && number3 > 0) {
+//   console.log('The number is positive');
+// } else if (!isNaN(number3) && number3 == 0) {
+//   console.log('The number is 0');
+// } else if (!isNaN(number3) && number3 > -5) {
+//   console.log('The number is greater than -5 and still negative');
+// } else {
+//   console.log('Enter a number');
+// }
+// console.log('The if...else if...else statement is easy');
+
+
+
+// const number4 = prompt('Enter a number: ');
+// if (number4 >= 0) {
+//   if (number4 == 0) {
+//     console.log('You entered number 0');
+//   } else {
+//     console.log('You entered a positive number');
+//   }
+// } else {
+//   console.log('You entered a negative number');
+// }
+// console.log('The nested if statement is a little bit confusing');
+
+
+// const number5 = prompt('Enter a number: ');
+// if (number5 > 0) {
+//   console.log('The number is positive');
+// }
+// if (number5 == 0) {
+//   console.log('The number is 0');
+// } else {
+//   console.log('The number is negative');
+// }
+// console.log('The if... if...else statement end');
+
+
+
+
+// let grade = prompt('Enter your score :');
+// let result = grade >= 50 ? 'pass' : 'fail';
+// console.log(`You ${result} the exam.`);
+// console.log('Ternary if statement example-1 end\n');
+// console.log('**********');
+
+
+// let age = 15;
+// let result;
+// if (age >= 18) {
+//   result = 'You are eligible to vote.';
+// } else {
+//   result = 'You are not eligible to vote yet.';
+// }
+// console.log(result);
+// console.log('Same operation with ternary now\n');
+
+
+// let age = 15;
+// let result =
+//   age >= 18 ? 'You are eligible to vote.' : 'You are not eligible to vote yet';
+// console.log(result);
+// console.log('Ternary if statement example-2 end\n');
+
+
+
+// let a = prompt('enter num');
+// result = a >= 0 ? (a == 0 ? 'zero' : 'positive') : 'negative';
+// console.log(`The number is ${result}.`);
+// console.log('Nested ternary if statement end\n');
+
+// // SWİTCH CASE
+
+
+// let a = 2;
+// switch (a) {
+//   case 1:
+//     a = 'one';
+//     break;
+//   case 2:
+//     a = 'two';
+//     break;
+//   default:
+//     a = 'not found';
+//     break;
+// }
+// console.log(`The value is ${a}`);
+ 
 
 
 
