c113c421da364e03210e417bb1b3e8426c4773b6
kamilsmr <kamils6035@gmail.com>
2021-05-11 09:13:07 +0300

    uptade

--- Kosullu-ifadeler/app.js
+++ Kosullu-ifadeler/app.js
@@ -16,3 +16,126 @@
 // let x = (y=5 , z=6);
 // console.log(x); // 6 döndü virgül ensondaki değeri döndürür.
 
+// const number = prompt('enter a number : ');
+// if (number>0){
+//     console.log("sayı sıfırdan büyüktür");
+// } else {
+//     console.log("sayı sıfır veya negatiftir.");
+// }
+
+//if kullanmadan da and operatörü ile yapabiliriz tek satır.
+// let x =true;
+// x && console.log("işlem başarılı"); 
+   
+// let number = prompt("lütfen bir sayı giriniz : ");
+// if (number>0){
+//     console.log("sayı pozitiftir.");
+// }else if(number == 0) {
+//     console.log("sayı sıfırdır.");
+// }else{ 
+//     console.log("sayı negatiftir.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
